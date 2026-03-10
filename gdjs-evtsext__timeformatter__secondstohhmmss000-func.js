47366c2fb453c57fa5151bef1d5606d8bdc9b4ff
SachaStar25 <sachastar25@gmail.com>
2026-03-10 18:21:58 +0300

    Add files via upload

--- gdjs-evtsext__timeformatter__secondstohhmmss000-func.js
+++ gdjs-evtsext__timeformatter__secondstohhmmss000-func.js
@@ -9,7 +9,7 @@ gdjs.evtsExt__TimeFormatter__SecondsToHHMMSS000 = {};
 gdjs.evtsExt__TimeFormatter__SecondsToHHMMSS000.idToCallbackMap = new Map();
 
 
-gdjs.evtsExt__TimeFormatter__SecondsToHHMMSS000.userFunc0xf6b6f0 = function GDJSInlineCode(runtimeScene, eventsFunctionContext) {
+gdjs.evtsExt__TimeFormatter__SecondsToHHMMSS000.userFunc0x151d2c8 = function GDJSInlineCode(runtimeScene, eventsFunctionContext) {
 var format_time = function (time_second) {
     date = new Date(null);
     date.setMilliseconds(1000*time_second);
@@ -27,7 +27,7 @@ gdjs.evtsExt__TimeFormatter__SecondsToHHMMSS000.eventsList0 = function(runtimeSc
 {
 
 
-gdjs.evtsExt__TimeFormatter__SecondsToHHMMSS000.userFunc0xf6b6f0(runtimeScene, eventsFunctionContext);
+gdjs.evtsExt__TimeFormatter__SecondsToHHMMSS000.userFunc0x151d2c8(runtimeScene, eventsFunctionContext);
 
 }
 
